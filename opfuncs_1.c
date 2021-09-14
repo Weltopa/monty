38a8866a0d3a44f144fc3960942c010e407ec370
Weltopa <patrick.welton@yahoo.com>
2021-09-14 15:02:43 -0700

    opcode functions test

---
+++ opfuncs_1.c
@@ -0,0 +1,10 @@
+#include "monty.h"
+
+/**
+ * pall - Prints all values of the stack.
+ * @shead: Stack head pointer.
+ * @line: line number.
+ * Return: Void.
+ */
+void pall(stack_t **shead, unsigned int line_number)
+{
