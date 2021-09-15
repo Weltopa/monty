da826822d1aa9a7b32fd8508478c2228839c39fa
Weltopa <patrick.welton@yahoo.com>
2021-09-15 07:48:46 -0700

    comments

--- main.c
+++ main.c
@@ -1,1 +1,10 @@
 #include "monty.h"
+
+/**
+ * main - Reads Monty files, creates from commands.
+ * @argc: Number of argumnents.
+ * @argv: Arg string.
+ * Return: Exit on success or failure.
+ */
+int main(int argc, char **argv)
+{
