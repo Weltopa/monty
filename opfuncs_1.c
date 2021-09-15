e3c670c68899c8d7b72fc6f62b8e2c558616c026
Weltopa <patrick.welton@yahoo.com>
2021-09-14 17:26:49 -0700

    pint test

--- opfuncs_1.c
+++ opfuncs_1.c
@@ -8,3 +8,20 @@
  */
 void pall(stack_t **shead, unsigned int line_number)
 {
+}
+
+/**
+ * pint - Prints value of stack head.
+ * @shead: Stack head of dub list.
+ * @line_number: Current line number.
+ * Return: Void.
+ */
+{
+
+	if (!shead)
+	{
+		dprintf(stderr, " L%d: can't pint, stack empty\n", line_number);
+		exit(EXIT_FAILURE);
+	}
+	printf("%d\n", (*shead)->);
+}
